001d5f62f60f284042f663a3e8224c4cdeaed0cb
Toufiq Shishir <toufiq_a24@outlook.com>
2025-08-19 23:16:10 +0600

    chore: spring boot project setup

---
+++ backend/src/main/java/io/github/twfksh/wisecamp/WisecampApplication.java
@@ -0,0 +1,13 @@
+package io.github.twfksh.wisecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisecampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisecampApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/twfksh/wisecamp/WisecampApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.twfksh.wisecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
